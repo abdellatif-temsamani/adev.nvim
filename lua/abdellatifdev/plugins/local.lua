532f59aa465072bf9e85529bbf8bab673debaa94
abdellatif-temsamani <contact@abdellatif.dev>
2024-12-10 17:27:53 +0100

    nvim

--- lua/abdellatifdev/plugins/local.lua
+++ lua/abdellatifdev/plugins/local.lua
@@ -1,7 +1,16 @@
+local function is_i3config()
+    return vim.uv.cwd() == vim.fn.getenv("HOME") .. "/dotfiles/i3/.config/i3"
+end
+
 return {
     {
         dir = "~/programming/twitch/tree-sitter-i3config/",
-        ft = {"i3config"},
+        event = { "BufEnter i3config" , "BufEnter grammar.js" },
+        lazy = not is_i3config(),
+
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter"
+        },
         config = function()
             ---@module "i3config"
             local i3config = require("i3config")
