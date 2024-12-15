455fb37b6ae80cdce38a73505425be25516d9a49
abdellatif-temsamani <contact@abdellatif.dev>
2024-12-15 17:31:55 +0100

    nvim

---
+++ lua/abdellatifdev/plugins/supermaven.lua
@@ -0,0 +1,14 @@
+return {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+        require("supermaven-nvim").setup({
+            keymaps = {
+                -- alt-l
+                accept_suggestion = "<M-l>",
+               clear_suggestion = "<C-]>",
+                accept_word = "<C-j>",
+
+            },
+        })
+    end,
+}
