14e15086ae3e6e73d3abba272b096ebb83908996
abdellatif-temsamani <contact@abdellatif.dev>
2024-12-22 19:01:52 +0100

    nvim

--- lua/abdellatifdev/plugins/blink-cmp.lua
+++ lua/abdellatifdev/plugins/blink-cmp.lua
@@ -23,6 +23,7 @@ return {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono'
         },
+
         snippets = {
             expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
             active = function(filter)
@@ -41,25 +42,31 @@ return {
                     name = "path",
                     enabled = true,
                     module = "blink.cmp.sources.path",
-                    score_offset = 1000,     -- the higher the number, the higher the priority
+                    score_offset = 1000, -- the higher the number, the higher the priority
                 },
                 lsp = {
                     name = "lsp",
                     enabled = true,
                     module = "blink.cmp.sources.lsp",
-                    score_offset = 1000,     -- the higher the number, the higher the priority
+                    score_offset = 1000, -- the higher the number, the higher the priority
                 },
                 luasnip = {
                     name = "luasnip",
                     enabled = true,
                     module = "blink.cmp.sources.luasnip",
-                    score_offset = 950,     -- the higher the number, the higher the priority
+                    score_offset = 950, -- the higher the number, the higher the priority
                 },
                 snippets = {
                     name = "snippets",
                     enabled = true,
                     module = "blink.cmp.sources.snippets",
-                    score_offset = 900,     -- the higher the number, the higher the priority
+                    score_offset = 900, -- the higher the number, the higher the priority
+                },
+                buffer = {
+                    name = "buffer",
+                    enabled = true,
+                    module = "blink.cmp.sources.buffer",
+                    score_offset = 500, -- the higher the number, the higher the priority
                 },
                 lazydev = {
                     name = "LazyDev",
--- lua/abdellatifdev/plugins/theme.lua
+++ lua/abdellatifdev/plugins/theme.lua
@@ -1,26 +1,10 @@
 return {
     {
-        "folke/tokyonight.nvim",
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
-        lazy = false,
         config = function()
-            ---@diagnostic disable-next-line: missing-fields
-            require("tokyonight").setup({
-                style = "night",
-                transparent = true,
-                terminal_colors = true,
-                dim_inactive = true,
-                lualine_bold = true
-            })
-            vim.cmd([[colorscheme tokyonight]])
-        end,
-    },
-
-    -- {
-    --     'uZer/pywal16.nvim',
-    --     config = function()
-    --         vim.cmd([[colorscheme pywal16]])
-    --     end,
-    -- }
-
+            vim.cmd [[ colorscheme catppuccin ]]
+        end
+    }
 }
