929785d3765a90c947a6a7b643b096877cb15101
abdellatif-temsamani <contact@abdellatif.dev>
2024-12-22 20:02:05 +0100

    nvim

--- lua/abdellatifdev/plugins/highlight-colors.lua
+++ lua/abdellatifdev/plugins/highlight-colors.lua
@@ -1,10 +1,17 @@
+local color = "red"
 return {
     'brenoprata10/nvim-highlight-colors',
     event = require("abdellatifdev.consts").file_event,
     lazy = true,
     opts = {
-        render = 'background',
+        render = 'virtual',
+        ---Set virtual symbol (requires render to be set to 'virtual')
+        virtual_symbol = '■',
+
+        ---Set virtual symbol suffix (defaults to '')
+        virtual_symbol_prefix = '',
         enable_named_colors = true,
         enable_tailwind = true,
     }
 }
+
