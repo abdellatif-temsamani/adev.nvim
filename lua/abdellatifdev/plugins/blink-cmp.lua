deff57d6b9ebe49db7c5afb05d7eb661e48f3a0f
abdellatif-temsamani <contact@abdellatif.dev>
2024-12-23 00:24:19 +0100

    nvim

--- lua/abdellatifdev/plugins/blink-cmp.lua
+++ lua/abdellatifdev/plugins/blink-cmp.lua
@@ -12,6 +12,7 @@ return {
         },
 
         completion = {
+            accept = { auto_brackets = { enabled = false }, },
             documentation = { auto_show = true, auto_show_delay_ms = 500 },
 
         },
