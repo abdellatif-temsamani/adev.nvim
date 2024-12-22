1c464ffaf346fbb28f2acf11bd67e4f6f5be64cb
abdellatif-temsamani <contact@abdellatif.dev>
2024-12-22 22:50:10 +0100

    nvim

--- lua/abdellatifdev/plugins/blink-cmp.lua
+++ lua/abdellatifdev/plugins/blink-cmp.lua
@@ -7,9 +7,6 @@ return {
     opts = {
         keymap = {
             preset = 'default',
-            ["<Tab>"] = { "snippet_forward", "fallback" },
-            ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
             ["<C-k>"] = { "scroll_documentation_up", "fallback" },
             ["<C-j>"] = { "scroll_documentation_down", "fallback" },
         },
@@ -22,17 +19,6 @@ return {
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono'
-        },
-
-        snippets = {
-            expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-            active = function(filter)
-                if filter and filter.direction then
-                    return require('luasnip').jumpable(filter.direction)
-                end
-                return require('luasnip').in_snippet()
-            end,
-            jump = function(direction) require('luasnip').jump(direction) end,
         },
 
         sources = {
