9c6d78037ff927653eb253bc80adda1fbe3141ac
abdellatif-temsamani <contact@abdellatif.dev>
2025-01-25 18:36:04 +0100

    snaks

--- lua/abdellatifdev/plugins/flutter.lua
+++ lua/abdellatifdev/plugins/flutter.lua
@@ -5,7 +5,6 @@ return {
         event = require('abdellatifdev.consts').events.file,
         dependencies = {
             'nvim-lua/plenary.nvim',
-            'stevearc/dressing.nvim',
         },
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
