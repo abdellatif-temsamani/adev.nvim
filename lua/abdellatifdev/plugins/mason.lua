d4942f2e9fe1415a64eb29f10c875be31ac68221
abdellatif Temsamani <contact@abdellatif.dev>
2025-02-04 12:23:43 +0100

    nvim

--- lua/abdellatifdev/plugins/mason.lua
+++ lua/abdellatifdev/plugins/mason.lua
@@ -1,100 +1,104 @@
 return {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    cmd = "Mason",
-    event = require("abdellatifdev.consts").events.pre,
-    opts = {
-        ui = {
-            check_outdated_packages_on_open = true,
-            border = "none",
-        },
-    },
-    dependencies = {
-        {
-            "jay-babu/mason-nvim-dap.nvim",
-            event = require("abdellatifdev.consts").events.pre,
-            opts = {
-                ensure_installed = {
-                    "bash-debug-adapter",
-                    "chrome-debug-adapter",
-                    "codelldb",
-                    "debugpy",
-
-                }
-            }
-        },
-        {
-            "jay-babu/mason-null-ls.nvim",
-            event = require("abdellatifdev.consts").events.pre,
-            dependencies = {
-                "williamboman/mason.nvim",
-                "nvimtools/none-ls.nvim",
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        cmd = "Mason",
+        event = require("abdellatifdev.consts").events.pre,
+        opts = {
+            ui = {
+                check_outdated_packages_on_open = true,
+                border = "none",
             },
-            opts = {
-                ensure_installed = {
-                    "actionlint",
-                    "checkmake",
-                    "djlint",
-                    "gitlint",
-                    "hadolint",
-                    "npm-groovy-lint",
-                    "phpcs",
-                    "phpmd",
-                    "proselint",
-                    "pyre",
-                    "shellharden",
-                    "yamllint",
-                    "alex",
-                }
-            }
         },
-        {
-            "williamboman/mason-lspconfig.nvim",
-            event = require("abdellatifdev.consts").events.pre,
-            opts = {
-                ensure_installed = {
-                    "phpactor",
-                    "intelephense",
-                    "lua_ls",
-                    "csharp_ls",
-                    "volar",
-                    "htmx",
-                    "ts_ls",
-                    "emmet_ls",
-                    "astro",
-                    "svelte",
-                    "svlangserver",
-                    "svls",
-                    "bashls",
-                    "clangd",
-                    "eslint",
-                    -- "cmake",
-                    "cssmodules_ls",
-                    "jdtls",
-                    "dockerls",
-                    "docker_compose_language_service",
-                    "html",
-                    "jsonls",
-                    "pyre",
-                    "pylsp",
-                    "pyright",
-                    "rust_analyzer",
-                    "sqlls",
-                    "stylelint_lsp",
-                    "cssls",
-                    "prismals",
-                    "tailwindcss",
-                    "taplo",
-                    "zls",
-                    "solidity",
-                    "solidity_ls_nomicfoundation",
-                    "asm_lsp",
-                    "yamlls",
-                    "lemminx",
-                    "texlab"
-                },
+
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = require("abdellatifdev.consts").events.pre,
+        after = "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "bash-debug-adapter",
+                "chrome-debug-adapter",
+                "codelldb",
+                "debugpy",
+
             }
         }
     },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = require("abdellatifdev.consts").events.pre,
+        after = "williamboman/mason.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        opts = {
+            ensure_installed = {
+                "actionlint",
+                "checkmake",
+                "djlint",
+                "gitlint",
+                "hadolint",
+                "npm-groovy-lint",
+                "phpcs",
+                "phpmd",
+                "proselint",
+                "pyre",
+                "shellharden",
+                "yamllint",
+                "alex",
+            }
+        }
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = require("abdellatifdev.consts").events.pre,
+        after = "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "phpactor",
+                "intelephense",
+                "lua_ls",
+                "csharp_ls",
+                "volar",
+                "htmx",
+                "ts_ls",
+                "emmet_ls",
+                "astro",
+                "svelte",
+                "svlangserver",
+                "svls",
+                "bashls",
+                "clangd",
+                "eslint",
+                -- "cmake",
+                "cssmodules_ls",
+                "jdtls",
+                "dockerls",
+                "docker_compose_language_service",
+                "html",
+                "jsonls",
+                "pyre",
+                "pylsp",
+                "pyright",
+                "rust_analyzer",
+                "sqlls",
+                "stylelint_lsp",
+                "cssls",
+                "prismals",
+                "tailwindcss",
+                "taplo",
+                "zls",
+                "solidity",
+                "solidity_ls_nomicfoundation",
+                "asm_lsp",
+                "yamlls",
+                "lemminx",
+                "texlab"
+            },
+        }
+    }
 
 }
