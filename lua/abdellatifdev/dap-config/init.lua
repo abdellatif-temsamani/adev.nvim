bd941c3a76cb70dd9347aa0ad7c7f4ee733266b9
abdellatif Temsamani <contact@abdellatif.dev>
2025-02-12 11:43:59 +0100

    nvim

--- lua/abdellatifdev/dap-config/init.lua
+++ lua/abdellatifdev/dap-config/init.lua
@@ -10,15 +10,6 @@ local js_based_languages = {
 local function setup()
     local dap = require("dap")
 
-    if vim.fn.filereadable(".vscode/launch.json") then
-        local dap_vscode = require("dap.ext.vscode")
-        dap_vscode.load_launchjs(nil, {
-            ["pwa-node"] = js_based_languages,
-            ["chrome"] = js_based_languages,
-            ["pwa-chrome"] = js_based_languages,
-        })
-    end
-
     require("abdellatifdev.dap-config.adapters.c_cpp_rust").setup(dap)
     require("abdellatifdev.dap-config.adapters.bash").setup(dap)
     require("abdellatifdev.dap-config.adapters.node").setup(dap)
