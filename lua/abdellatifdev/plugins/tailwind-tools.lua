8638b9b7b8bdda1daf76bf2cb44d5230c93636ef
abdellatif Temsamani <contact@abdellatif.dev>
2025-04-02 12:52:12 +0000

    nvim

--- lua/abdellatifdev/plugins/tailwind-tools.lua
+++ lua/abdellatifdev/plugins/tailwind-tools.lua
@@ -7,7 +7,16 @@ return {
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "nvim-telescope/telescope.nvim", -- optional
-        "neovim/nvim-lspconfig",     -- optional
+        "neovim/nvim-lspconfig",         -- optional
     },
-    opts = {}                        -- your configuration
+    opts = {
+        conceal = {
+            enabled = true, -- can be toggled by commands
+            min_length = nil, -- only conceal classes exceeding the provided length
+            symbol = "󱏿", -- only a single character is allowed
+            highlight = { -- extmark highlight options, see :h 'highlight'
+                fg = "#38BDF8",
+            },
+        },
+    } -- your configuration
 }
