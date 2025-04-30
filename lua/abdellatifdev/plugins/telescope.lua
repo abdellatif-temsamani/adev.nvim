d4a733eb2cce215d502632421eff9f88937b14bb
abdellatif Temsamani <contact@abdellatif.dev>
2025-04-30 13:43:13 +0000

    nvim

--- lua/abdellatifdev/plugins/telescope.lua
+++ lua/abdellatifdev/plugins/telescope.lua
@@ -43,7 +43,8 @@ return {
             },
         },
         extensions = {
-            fzf = {}
+            fzf       = {},
+            worktrees = {}
         },
         defaults = {
             layout_strategy = 'horizontal',
