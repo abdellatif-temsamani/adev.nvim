3a9abed92847a465a7311ce9b89b72b11830c8a0
abdellatif Temsamani <contact@abdellatif.dev>
2025-05-14 10:16:30 +0000

    nvim

--- lua/abdellatifdev/plugins/treesitter.lua
+++ lua/abdellatifdev/plugins/treesitter.lua
@@ -22,23 +22,6 @@ local function treesitter_setup()
             end,
         },
     })
-
-    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-    ---@diagnostic disable-next-line: inject-field
-    parser_config.blade = {
-        install_info = {
-            url = "https://github.com/EmranMR/tree-sitter-blade",
-            files = { "src/parser.c" },
-            branch = "main",
-        },
-        filetype = "blade",
-    }
-
-    vim.filetype.add({
-        pattern = {
-            [".*%.blade%.php"] = "blade",
-        },
-    })
 end
 
 return {
