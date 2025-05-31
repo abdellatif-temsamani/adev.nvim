c0bb918243f9e532d79b0af785adafec8f500ea8
abdellatif Temsamani <contact@abdellatif.dev>
2025-05-31 15:48:33 +0000

    nvim

--- lua/abdellatifdev/plugins/blink-cmp.lua
+++ lua/abdellatifdev/plugins/blink-cmp.lua
@@ -1,54 +1,73 @@
 return {
-    'saghen/blink.cmp',
-    -- build = "cargo build --release",
-    version = '*',
-    dependencies = {
-        'rafamadriz/friendly-snippets',
-        'honza/vim-snippets',
+    {
+        'saghen/blink.compat',
+        version = '2.*',
+        lazy = true,
+        opts = {},
     },
-    opts = {
-        keymap = {
-            preset = 'default',
-            ["<C-k>"] = { "scroll_documentation_up", "fallback" },
-            ["<C-j>"] = { "scroll_documentation_down", "fallback" },
+    {
+        'saghen/blink.cmp',
+        version = '*',
+        dependencies = {
+            'rafamadriz/friendly-snippets',
+            'honza/vim-snippets',
+            'dmitmel/cmp-digraphs'
         },
-
-        completion = {
-            list = { selection = { preselect = true, auto_insert = true } },
-            accept = { auto_brackets = { enabled = false }, },
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 500,
-                indow = {
-                    border = 'single'
-                }
-            },
-            menu = {
-                border = 'single',
-                draw = {
-                    columns = { { "label", gap = 1, "kind_icon" } },
-                }
+        opts = {
+            keymap = {
+                preset = 'default',
+                ["<C-k>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-j>"] = { "scroll_documentation_down", "fallback" },
             },
 
-        },
+            completion = {
+                list = { selection = { preselect = true, auto_insert = true } },
+                accept = { auto_brackets = { enabled = false }, },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                    window = {
+                        border = 'single'
+                    }
+                },
+                menu = {
+                    border = 'single',
+                    draw = {
+                        columns = { { "label", gap = 1, "kind_icon" } },
+                    }
+                },
 
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer', },
-        },
-
-        snippets = { preset = 'luasnip' },
-
-        per_filetype = {
-            lua = { inherit_defaults = true, 'lazydev' },
-        },
-        providers = {
-            lazydev = {
-                name = "LazyDev",
-                module = "lazydev.integrations.blink",
-                score_offset = 100,
             },
+
+            sources = {
+                default = {
+                    'lsp',
+                    'path',
+                    'snippets',
+                    'buffer',
+                    'laravel',
+                    'cmdline',
+                    'omni'
+                },
+                per_filetype = {
+                    lua = { inherit_defaults = true, 'lazydev' },
+                },
+                providers = {
+                    laravel = {
+                        name = "laravel",
+                        module = "blink.compat.source",
+                    },
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        score_offset = 100,
+                    },
+                },
+            },
+
+            snippets = { preset = 'luasnip' },
         },
-    },
-    signature = { enabled = true },
-    opts_extend = { "sources.default" },
+        signature = { enabled = true },
+        opts_extend = { "sources.default" },
+    }
 }
