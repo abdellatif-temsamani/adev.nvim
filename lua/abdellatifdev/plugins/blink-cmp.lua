0bb182b0d6ead35221aea85a80497318a9051fa4
abdellatif Temsamani <contact@abdellatif.dev>
2025-06-02 13:29:24 +0100

    nvim

--- lua/abdellatifdev/plugins/blink-cmp.lua
+++ lua/abdellatifdev/plugins/blink-cmp.lua
@@ -12,61 +12,73 @@ return {
             'rafamadriz/friendly-snippets',
             'honza/vim-snippets',
         },
-        opts = {
-            keymap = {
-                preset = 'default',
-                ["<C-k>"] = { "scroll_documentation_up", "fallback" },
-                ["<C-j>"] = { "scroll_documentation_down", "fallback" },
-            },
+        opts = function()
+            local plugins = require("lazy.core.config").plugins
+            local lazydev_enabled = plugins["lazydev.nvim"] ~= nil and plugins["lazydev.nvim"].cond()
 
-            completion = {
-                list = { selection = { preselect = true, auto_insert = true } },
-                accept = { auto_brackets = { enabled = false }, },
-                documentation = {
-                    auto_show = true,
-                    auto_show_delay_ms = 500,
-                    window = {
-                        border = 'single'
-                    }
-                },
-                menu = {
-                    border = 'single',
-                    draw = {
-                        columns = { { "kind_icon", gap = 1, "label", } },
-                    }
+            return {
+                cmdline = {
+                    keymap = { preset = 'inherit' },
+                    completion = { menu = { auto_show = true } },
                 },
 
-            },
+                keymap = {
+                    preset = 'default',
+                    ["<C-k>"] = { "scroll_documentation_up", "fallback" },
+                    ["<C-j>"] = { "scroll_documentation_down", "fallback" },
+                },
 
-            sources = {
-                default = {
-                    'lsp',
-                    'path',
-                    'snippets',
-                    'buffer',
-                    'laravel',
-                    'cmdline',
-                    'omni'
-                },
-                per_filetype = {
-                    lua = { inherit_defaults = true, 'lazydev' },
-                },
-                providers = {
-                    laravel = {
-                        name = "laravel",
-                        module = "blink.compat.source",
+                completion = {
+                    list = { selection = { preselect = true, auto_insert = true } },
+                    accept = { auto_brackets = { enabled = false } },
+                    documentation = {
+                        auto_show = true,
+                        auto_show_delay_ms = 500,
+                        window = {
+                            border = 'single'
+                        }
                     },
-                    lazydev = {
-                        name = "LazyDev",
-                        module = "lazydev.integrations.blink",
-                        score_offset = 100,
+                    menu = {
+                        border = 'single',
+                        draw = {
+                            columns = { { "kind_icon", gap = 1, "label" } },
+                        }
                     },
                 },
-            },
 
-            snippets = { preset = 'luasnip' },
-        },
-        signature = { enabled = true },
-        opts_extend = { "sources.default" },
+                sources = {
+                    default = {
+                        'lsp',
+                        'path',
+                        'snippets',
+                        'buffer',
+                        'laravel',
+                        'cmdline',
+                        'omni',
+                    },
+                    per_filetype = {
+                        lua = lazydev_enabled
+                            and { inherit_defaults = true, 'lazydev' }
+                            or { inherit_defaults = true },
+                    },
+                    providers = vim.tbl_extend("force", {
+                        laravel = {
+                            name = "laravel",
+                            module = "blink.compat.source",
+                        },
+                    }, lazydev_enabled and {
+                        lazydev = {
+                            name = "LazyDev",
+                            module = "lazydev.integrations.blink",
+                            score_offset = 100,
+                        }
+                    } or {}),
+                },
+
+                snippets = { preset = 'luasnip' },
+                signature = { enabled = true },
+                opts_extend = { "sources.default" },
+            }
+        end
     }
 }
