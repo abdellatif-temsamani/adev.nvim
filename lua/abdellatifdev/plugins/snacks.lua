0ac4e413cb5ddb2d2ca309cbc00ed09c76d37f6b
abdellatif Temsamani <contact@abdellatif.dev>
2025-07-30 21:30:28 +0100

    feat(snacks): add image support
    Added ability to preview images in Neovim.

--- lua/abdellatifdev/plugins/snacks.lua
+++ lua/abdellatifdev/plugins/snacks.lua
@@ -60,6 +60,7 @@ return {
         },
         input        = { enabled = true },
         lazygit      = { enabled = true },
+        image        = { enabled = true },
         statuscolumn = {
             enabled = true,
             left = { "mark", "sign" },
