bd57ce87233c1264aa3f43596ea15bf68cb57869
abdellatif Temsamani <contact@abdellatif.dev>
2025-08-02 01:15:34 +0100

    refactor(comments): change config

--- lua/abdellatifdev/plugins/comments.lua
+++ lua/abdellatifdev/plugins/comments.lua
@@ -1,7 +1,5 @@
 return {
     'numToStr/Comment.nvim',
-    event = require("abdellatifdev.consts").events.file,
-    opts = {
-        -- add any options here
-    }
+    event = require("abdellatifdev.consts").events.pre,
+    opts = {}
 }
