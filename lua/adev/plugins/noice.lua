2fa949df7cefb812183a498b2a0baa7a81eabf14
abdellatif Temsamani <contact@abdellatif.dev>
2025-08-25 12:51:10 +0100

    feat(noice): override defaults

--- lua/adev/plugins/noice.lua
+++ lua/adev/plugins/noice.lua
@@ -3,6 +3,13 @@ return
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true, -- if you're using nvim-cmp
+            },
+        },
         notify = {
             enabled = true,
         },
