d09760a8284a25d77dd845640a512be540b91ad5
abdellatif Temsamani <contact@abdellatif.dev>
2025-09-15 16:05:43 +0100

    fix(laravel)!: fix commands

--- lua/adev/plugins/laravel.lua
+++ lua/adev/plugins/laravel.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 return {
     "adalessa/laravel.nvim",
     cond = function()
@@ -13,9 +14,15 @@ return {
         "nvim-neotest/nvim-nio",
     },
     keys = {
-        { "<leader>la", ":Laravel artisan<cr>", desc = "artisan commands" },
-        { "<leader>lr", ":Laravel routes<cr>",  desc = "artisan route:list" },
-        { "<leader>lm", ":Laravel make<cr>",    desc = "artisan make" },
+        { "<leader>ll", function() Laravel.pickers.laravel() end,              desc = "Laravel nvim commands" },
+        { "<leader>la", function() Laravel.pickers.artisan() end,              desc = "Artisan Picker" },
+        { "<leader>lt", function() Laravel.commands.run("actions") end,        desc = "Actions Picker" },
+        { "<leader>lr", function() Laravel.pickers.routes() end,               desc = "Routes Picker" },
+        { "<leader>lh", function() Laravel.run("artisan docs") end,            desc = "Documentation" },
+        { "<leader>lm", function() Laravel.pickers.make() end,                 desc = "Make Picker" },
+        { "<leader>lc", function() Laravel.pickers.commands() end,             desc = "Commands Picker" },
+        { "<leader>lo", function() Laravel.pickers.resources() end,            desc = "Resources Picker" },
+        { "<leader>lp", function() Laravel.commands.run("command_center") end, desc = "Command Center" },
     },
     opts = {
         lsp_server = "intelephense",
