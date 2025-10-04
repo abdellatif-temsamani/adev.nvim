7b1d5bd419d724e224ee9abac6d7d6c832dc4956
abdellatif Temsamani <contact@abdellatif.dev>
2025-10-04 16:54:10 +0100

    feat(autocmd):gdscript space

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,7 +1,17 @@
 local autocmd     = vim.api.nvim_create_autocmd
 local general_grp = vim.api.nvim_create_augroup("GENERAL", { clear = true })
 
-vim.api.nvim_create_autocmd("VimEnter", {
+autocmd("FileType", {
+    group    = general_grp,
+    pattern  = "gdscript",
+    callback = function()
+        vim.opt_local.expandtab = true -- use spaces
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.tabstop = 4
+    end,
+})
+
+autocmd("VimEnter", {
     group    = general_grp,
     callback = function()
         require("adev.utils").check_adev_update()
