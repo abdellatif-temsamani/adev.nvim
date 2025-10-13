1c70603f57914fbcb6fb22263546e4ab4ce0c49c
abdellatif Temsamani <contact@abdellatif.dev>
2025-10-13 11:07:16 +0100

    fix(comments): lazyloading conditions

--- lua/adev/plugins/comments.lua
+++ lua/adev/plugins/comments.lua
@@ -2,6 +2,6 @@ local events = require "adev.utils.consts.events"
 
 return {
     "numToStr/Comment.nvim",
-    event = { events.buffer.new_file },
+    event = { events.buffer.new_file, events.buffer.file_pre },
     opts = {},
 }
