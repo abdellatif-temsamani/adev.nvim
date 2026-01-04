48914d8e1717a57d6bb0168fb7bdddd5e3bcfef8
abdellatif Temsamani <contact@abdellatif.dev>
2026-01-04 22:06:18 +0100

    refactor(adev-files): refactor list files to open in a floating window

--- lua/adev-common/ui/window.lua
+++ lua/adev-common/ui/window.lua
@@ -11,4 +11,32 @@ function M.create_buf(content, listed, scratch)
     return buf
 end
 
+--- @class FloatingWinOpts
+--- @field width? integer
+--- @field height? integer
+
+--- @class FloatingWinOpts
+M.floating_defaults = {
+    width = 40,
+    height = 5,
+}
+
+--- create a floating window for a buffer
+---@param buf integer buffer id
+---@param opts? FloatingWinOpts
+function M:new_floating(buf, opts)
+    --- @class FloatingWinOpts
+    opts = vim.tbl_deep_extend("force", {}, self.floating_defaults, opts or {})
+
+    return vim.api.nvim_open_win(buf, true, {
+        relative = "editor",
+        width = opts.width,
+        height = opts.height,
+        row = math.floor((vim.o.lines - opts.height) / 2),
+        col = math.floor((vim.o.columns - opts.width) / 2),
+        style = "minimal",
+        border = Adev.ui.border,
+    })
+end
+
 return M
