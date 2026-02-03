a66e8ac542bb9ab53718eea106fb7cf218a83777
abdellatif Temsamani <contact@abdellatif.dev>
2026-02-03 12:26:02 +0100

    plugin("markdown"): add render-markdown

---
+++ lua/adev/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+    -- NOTE: optional system deps: libtexprintf, pylatexenc
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}
